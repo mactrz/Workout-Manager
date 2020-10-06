07f89c86e1e59923bd3fd654ed9691d6495ba09b
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-10-06 10:54:42 +0200

    zad2

--- lab1/src/Form.js
+++ lab1/src/Form.js
@@ -4,8 +4,19 @@ import { Formik } from "formik";
 function Forma({changePrzeslany}) {
     return(
         <div>
-            <Formik initialValues={{text: '', date: ''}} onSubmit={(data) => changePrzeslany(data)}>
-                {({values, handleChange, handleSubmit}) => (
+            <Formik initialValues={{text: '', date: ''}} onSubmit={(data) => changePrzeslany(data)} 
+            validate={(val) => {
+                const aktualnaData = new Date();
+                let errors = {};
+                if(!val.text) {
+                    errors.text = "Nie moze byc puste";
+                }
+                else if(Date.parse(val.date) <= aktualnaData) {
+                    errors.date = "Data musi byc pozniejsza niz dzien dzisiejszy"
+                }
+                return errors
+            }}>
+                {({values, handleChange, handleSubmit, errors, touched}) => (
                     <form onSubmit={handleSubmit}>
                         <label>
                             ToDo
@@ -14,6 +25,7 @@ function Forma({changePrzeslany}) {
                             type="text"
                             onChange={handleChange}
                         />
+                        {errors.text && touched.text && <div>{errors.text}</div>}
                         </label><br/>
                         <label>
                             Data
@@ -22,6 +34,7 @@ function Forma({changePrzeslany}) {
                             type="date"
                             onChange={handleChange}
                         />
+                        {errors.date && touched.date && <div>{errors.date}</div>}
                         </label><br/>
                         <button type="submit">Zatwierdź</button>
                     </form>
